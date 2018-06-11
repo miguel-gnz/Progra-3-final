afce040cba04492ba3ddd3169b537f6b2951dd12
miguel-gnz <39819432+miguel-gnz@users.noreply.github.com>
2018-06-10 22:52:34 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,145 +1,134 @@
-#include <iostream>
-#include <stdlib.h>
-#include <string>
+﻿#include <iostream>
+#include <Windows.h>
+
 
 using namespace std;
 
 
-struct nodo
+
+void gotoxy(int x, int y)
 {
-	string nombre_arch;
-	string usuario;
-	int nro;
-	struct nodo *sgte;
-};
-
-struct cola {
-	nodo *delante;
-	nodo *atras;
-};
-
-void encolar(struct cola &q, int valor, string user, string nombre)
-{
-	struct nodo *aux = new(struct nodo);
-
-	aux->usuario = user;
-	aux->nombre_arch = nombre;
-	aux->nro = valor;
-	aux->sgte = NULL;
-
-	if (q.delante == NULL)
-		q.delante = aux;
-	else
-		(q.atras)->sgte = aux;
-
-	q.atras = aux;
+	HANDLE hcon;
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hcon, dwPos);
 }
 
+HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
-//desencolar elemento
-int desencolar(struct cola &q)
-{
-	int num;
-	string user;
-	string archivo;
-	struct nodo *aux;
+int b[5][5];
 
-	aux = q.delante;
-	num = aux->nro;
-	user = aux->usuario;
-	archivo = aux->nombre_arch;
+//PROTOTIPOS
 
-	q.delante = (q.delante)->sgte;
-	delete(aux);
-
-	return num;
-
-}
-//mostrar cola
-void muestraCola(struct cola q)
-{
-	struct nodo *aux;
-	aux = q.delante;
-
-	while (aux != NULL)
-	{
-		cout << "\n------------------------------------------------------------------------------------------------\n";
-		cout << "Usuario: " << aux->usuario << "\t Direccion de memoria-> " << &aux->usuario;
-		cout << "\nNombre de archivo: " << aux->nombre_arch << "\t Direccion de memoria-> " << &aux->usuario;
-		cout << "\nNumero de Hojas: " << aux->nro << "\t Direccion de memoria-> " << &aux->usuario;
-
-		aux = aux->sgte;
-	}
-}
-
-// Eliminar todos los elementos de la cola
-void vaciaCola(struct cola &q)
-{
-	struct nodo *aux;
-	while (q.delante != NULL)
-	{
-		aux = q.delante;
-		q.delante = aux->sgte;
-		delete(aux);
-	}
-	q.delante = NULL;
-	q.atras = NULL;
-}
-// menu de opciones
-void menu()
-{
-	cout << "\n\t SIMULADOR DE COLA DE IMPRESIONES\n\n";
-	cout << "1. ENCOLAR          " << endl;
-	cout << "2. DESENCOLAR       " << endl;
-	cout << "3. MOSTRAR COLA       " << endl;
-	cout << "4. VACIAR COLA         " << endl;
-	cout << "5. SALIR                " << endl;
-
-	cout << " \n INGRESE OPCION:     ";
-}
+void floyds(int b[][5]);
+void inicio();
+void ingreso();
 
 
 int main()
 {
-	struct cola q;
-	q.delante = NULL;
-	q.atras = NULL;
 
-	int dato;
-	int op;
-	int x;
+	
+	inicio();
 
-	string nombre;
-	string usuario;
+	system("cls");
+	
+	ingreso();
 
-	system("color 0b");
-	do {
-		menu(); cin >> op;
-		switch (op)
+	system("cls");
+
+	floyds(b);
+
+	gotoxy(20, 22);  system("pause");
+
+	return 0;
+}
+
+
+void floyds(int b[][5])
+{
+	int i, j, k;
+	for (k = 0; k < 5; k++)
+	{
+		for (i = 0; i < 5; i++)
 		{
-		case 1:
-			cout << "\n Usuario: "; cin >> usuario;
-			cout << "\n Nombre de archivo: "; cin >> nombre;
-			cout << "\n Numero de Hojas: "; cin >> dato;
-			encolar(q, dato, usuario, nombre);
-			cout << "\n\n\t\tDatos  " << "  ingresados...\n\n";
-			break;
-		case 2:
-			x = desencolar(q);
-			cout << "\n\n\t\tDatos  " << x << "  eliminados...\n\n";
-			break;
-		case 3:
-			cout << "\n\n MOSTRANDO COLA DE IMPRESION \n\n";
-			if (q.delante != NULL) muestraCola(q);
-			else cout << " \n\n\t Cola vacia...!" << endl;
-			break;
-		case 4:
-			vaciaCola(q);
-			cout << "\n\n\t\t Hecho....\n\n";
-			break;
+			for (j = 0; j < 5; j++)
+			{
+				if ((b[i][k] * b[k][j] != 0) && (i != j))
+				{
+					if ((b[i][k] + b[k][j] < b[i][j]) || (b[i][j] == 0))
+					{
+						b[i][j] = b[i][k] + b[k][j];
+					}
+				}
+			}
 		}
-		cout << endl << endl;
-		system("pause"); system("cls");
+	}
 
-	} while (op != 5);
+	system("cls");
+
+	SetConsoleTextAttribute(h, FOREGROUND_BLUE);
+	cout << "\t\t\tRESULTADO\n\n\n";
+
+
+	SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_BLUE);
+	for (i = 0; i < 5; i++)
+	{
+		gotoxy(15, 8 + i);
+		cout << "\t";
+		for (j = 0; j < 5; j++)
+		{
+			cout  << b[i][j] << "\t";
+		}
+	}
+}
+
+
+void inicio()
+{
+	system("cls");
+
+	SetConsoleTextAttribute(h, FOREGROUND_BLUE);
+	gotoxy(20, 2);  cout << "GRAFOS (TERCER PROYECTO PARCIAL)";
+
+	SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_BLUE);
+	gotoxy(4, 8);  cout << "-Bienvenido, el siguiente programa le permitira digitar los elementos"; 
+	gotoxy(4, 9);  cout << "de una matriz de adyacencia generada por los datos de un grafo de 5 nodos.";
+	gotoxy(4, 11);  cout << "-Como resultado sera generada una nueva matriz indicando la ruta mas corta";
+	gotoxy(4, 12);  cout << "hacia cada nodo por medio del metodo Floyd Warshall.";
+
+	SetConsoleTextAttribute(h, FOREGROUND_BLUE | FOREGROUND_RED);
+	gotoxy(60, 21);  cout << "Miguel Gonzalez";
+	gotoxy(60, 22);  cout << "Franklyn Escobar";
+	gotoxy(60, 23);  cout << "Bryan Portillo";
+
+	SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_BLUE);
+	gotoxy(20, 17);  system("pause");
+}
+
+void ingreso()
+{
+	system("cls");
+
+
+	SetConsoleTextAttribute(h, FOREGROUND_BLUE);
+	cout << "\t\t\tINGRESO DE VALORES PARA LA MATRIZ DE ADYACENCIA\n\n\n";
+
+
+	SetConsoleTextAttribute(h, FOREGROUND_GREEN | FOREGROUND_BLUE);
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Fila " << (i + 1) << "\n";
+		for (int j = 0; j < 5; j++)
+			
+		{
+			cin >> b[i][j];
+			
+		}
+	}
+
+
+	system("pause");
 }
